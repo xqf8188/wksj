654dc9946c1012fc61c2ca26cda6dfa1946775c0
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-02-27 20:31:33 +0800

    Update tanhua.js

--- tanhua/tanhua.js
+++ tanhua/tanhua.js
@@ -5,7 +5,7 @@ var body = {
     "mobile" : "18866889999",
     "status" : 1,
     "is_vip" : 1,
-    "member_name" : "作者...",
+    "member_name" : "怪我咯",
     "head_pic" : "https://s3.ax1x.com/2021/01/26/sOLQKA.jpg",
     "parent_id" : 22,
     "user_viptime" : "2088-06-06"
