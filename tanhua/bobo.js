cf577235dc08283d671621e0ac643c41b7b942f6
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-03-07 03:20:29 +0800

    Update bobo.js

--- tanhua/bobo.js
+++ tanhua/bobo.js
@@ -8,23 +8,4 @@
 #本地脚本
 
 #波波视频
-http://api.wscdn.vip(.+) url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/bobo
-
-[MITM]
-hostname = api.wscdn.vip,
-
-*/
-
-const path1 = "/index/User/ucenter";
-const path2 = "/index/Get/videoInfo";
-
-let obj = JSON.parse($response.body);
-
-if ($request.url.indexOf(path1) != -1){
-obj.data.isVip = "1",
-obj.data.isVip_forever = "1"
-}
-if ($request.url.indexOf(path2) != -1){
-obj.data.buyStatus = "1"
-}
-$done({body: JSON.stringify(obj)});
+http://api.wscdn.vip(.+) url script-response-body https://raw.githubusercontent.com/xqf8188/wksj/main/tanhua/bobo.js
