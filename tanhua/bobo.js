05aa63d56c01a91d68356f1454f3984a2702c552
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-04-10 06:49:56 +0800

    Update bobo.js

--- tanhua/bobo.js
+++ tanhua/bobo.js
@@ -1,14 +1,15 @@
 /*
 作者@king
+Tg群：https://t.me/yqc_777
 波波视频（lsp）
 下载地址:
-/3kQb6qo
+https://bit.ly/3kQb6qo
 [Qx]
 [rewrite_local]
 #本地脚本
 
 #波波视频
-http://api.wscdn.vip(.+) url script-response-body https://raw.githubusercontent.com/xqf8188/wksj/main/tanhua/bobo.js
+http://api.wscdn.vip(.+) url script-response-body bobo.js
 
 [MITM]
 hostname = api.wscdn.vip,
