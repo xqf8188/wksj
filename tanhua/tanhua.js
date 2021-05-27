3e00d205de93fccd237e54da25a59936b32b90dc
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-05-27 13:26:41 +0800

    Update tanhua.js

--- tanhua/tanhua.js
+++ tanhua/tanhua.js
@@ -6,7 +6,7 @@ var body = {
     "status" : 1,
     "is_vip" : 1,
     "member_name" : "怪我咯",
-    "head_pic" : "http://1008610010.laikanshu.top/uploads/admin/2021/01/26/sOLQKA.jpg",
+    "head_pic" : "http://1008610010.laikanshu.top/2021/01/26/sOLQKA.jpg",
     "parent_id" : 22,
     "user_viptime" : "2088-06-06"
   }
