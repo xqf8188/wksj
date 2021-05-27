88e005eeab2b89639eeac1286e24768a4b598914
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-05-27 13:36:08 +0800

    Update tanhua.js

--- tanhua/tanhua.js
+++ tanhua/tanhua.js
@@ -2,10 +2,10 @@ var body = {
   "status": "200",
   "msg": "返回成功",
   "data": {
-    "member_name": "180****4707",
-    "mobile": "18018374707",
+    "member_name": "188****9999",
+    "mobile": "18866889999",
     "head_pic": "http://1008610010.laikanshu.top/uploads/admin/202010/5f9b91e0a6fe0.jpg",
-    "parent_id": 738,
+    "parent_id": 22,
     "user_viptime": "2088-06-06",
     "status": 1,
     "is_vip": 1
