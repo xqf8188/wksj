6dc8cb7d04a1a9bed2b3ecbbc63498907afd67f1
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-05-27 13:31:22 +0800

    Update tanhua.js

--- tanhua/tanhua.js
+++ tanhua/tanhua.js
@@ -1,14 +1,14 @@
 var body = {
-  "status" : "200",
-  "msg" : "返回成功",
-  "data" : {
-    "mobile" : "18866889999",
-    "status" : 1,
-    "is_vip" : 1,
-    "member_name" : "怪我咯",
-    "head_pic" : "http://1008610010.laikanshu.top/2021/01/26/sOLQKA.jpg",
-    "parent_id" : 22,
-    "user_viptime" : "2088-06-06"
+  "status": "200",
+  "msg": "返回成功",
+  "data": {
+    "member_name": "180****4707",
+    "mobile": "18018374707",
+    "head_pic": "http://1008610010.laikanshu.top/uploads/admin/202010/5f9b91e0a6fe0.jpg",
+    "parent_id": 738,
+    "user_viptime": "2088-06-06",
+    "status": 1,
+    "is_vip": 1
   }
 };
 
