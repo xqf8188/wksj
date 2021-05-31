d4a32cbac4734bda075b6b47649012d85c46bd7f
xqf8188 <69407085+xqf8188@users.noreply.github.com>
2021-06-01 07:11:31 +0800

    Update ysm777.js

--- ysm777.js
+++ ysm777.js
@@ -8,12 +8,12 @@
 注意事项:重写不需要关闭，鉴权文章阅读不会被重写，若跳转了微信文章页面，那这个阅读应该是鉴权文章；对于之前脚本跑28、29、30等篇数就被限制的情况，用重写辅助脚本时可留意下是否这些篇数就会进入微信文章页面
 
 [rewrite_local]
-^http://.+/task/read\? url script-response-header https://raw.githubusercontent.com/age174/-/main/fqkk_auto_read.js
-^http://.+/mock/read\? url script-analyze-echo-response https://raw.githubusercontent.com/age174/-/main/fqkk_auto_read.js
+^http://.+/task/read\? url script-response-header https://raw.githubusercontent.com/xqf8188/wksj/main/ysm777.js
+^http://.+/mock/read\? url script-analyze-echo-response https://raw.githubusercontent.com/xqf8188/wksj/main/ysm777.js
 
 Loon：自测不行，不知道是Loon的问题还是写法与qx有不同之处；有使用Loon的，自行试试吧
-http-response ^http://.+/task/read\? script-path=https://raw.githubusercontent.com/age174/-/main/fqkk_auto_read.js, requires-body=false, timeout=10, tag=阅读文章重写
-http-request ^http://.+/mock/read\? script-path=https://raw.githubusercontent.com/age174/-/main/fqkk_auto_read.js, requires-body=true, timeout=10, tag=阅读返回重写
+http-response ^http://.+/task/read\? script-path= https://raw.githubusercontent.com/xqf8188/wksj/main/ysm777.js, requires-body=false, timeout=10, tag=阅读文章重写
+http-request ^http://.+/mock/read\? script-path= https://raw.githubusercontent.com/xqf8188/wksj/main/ysm777.js, requires-body=true, timeout=10, tag=阅读返回重写
 
 */
 
